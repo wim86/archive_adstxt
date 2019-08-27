b5402fbe2c99fa635b7a0d9d4a7ea728bb3d4b63
wim86 <wimhermans2@hotmail.com>
2019-08-28 00:08:16 +0300

    file split per 5k items

--- archive_adstxt/pipelines.py
+++ archive_adstxt/pipelines.py
@@ -11,16 +11,22 @@ class ArchiveAdstxtPipeline(object):
     def __init__(self):
         self.writer = None
         self.file = None
+        self.filename = None
+        self.counter = 0
 
     def open_spider(self, spider):
-        filename = "test_output.csv"
-        self.file = open(filename, 'w')
+        self.filename = "test_output.csv"
+        self.file = open(self.filename + '_' + str(self.counter), 'w')
         self.writer = csv.writer(self.file)
         self.writer.writerow(["domain"])
 
     def process_item(self, item, spider):
+        self.counter += 1
         domain = item['domain']
         self.writer.writerow([domain])
+        if self.counter % 5000 == 0:
+            self.file.close()
+            self.file = open(self.filename + '_' + str(self.counter), 'w')
         return item
 
     def close_spider(self, spider):
